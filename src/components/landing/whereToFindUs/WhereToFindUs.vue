f40342c6e7eb569ee27dd51eeaf0fdf6d57ef1c3
LnrdRmj <leonardo.ra14@icloud.com>
2025-03-11 15:37:40 +0100

    added links to places

--- src/components/landing/whereToFindUs/WhereToFindUs.vue
+++ src/components/landing/whereToFindUs/WhereToFindUs.vue
@@ -4,8 +4,30 @@ import LateralLogo from './LateralLogo.vue';
 import { useTemplateRef } from 'vue';
 
 const locations = [
-    'Locanda alle Vigne, Cormons (GO)',
-    'Osteria agli Antenati, San Lorenzo Isontino (GO)'
+    {
+        address: 'Locanda alle Vigne, Cormons (GO)',
+        url: 'https://locandallevigne.it/',
+    },
+    {
+        address: 'Osteria agli Antenati, San Lorenzo Isontino (GO)',
+        url: 'https://www.facebook.com/trattoriaAgliAntenati/',
+    },
+    {
+        address: 'Orsone, Cividale del Friuli (UD)',
+        url: 'https://orsonerelais.com/nuovo-sito-online/',
+    },
+    {
+        address: 'Osteria Baldovino, Ipplis (UD)',
+        url: 'https://www.thefork.it/ristorante/osteria-baldovino-r833387',
+    },
+    {
+        address: 'Eno-Therapy, Champoluc (AO)',
+        url: 'https://enotherapy.it/',
+    },
+    {
+        address: 'Alla Bettolina Ristorante, Frachey (AO)',
+        url: 'https://allabettolina.superbexperience.com/',
+    }
 ]
 
 const root = useTemplateRef('root')
@@ -21,7 +43,7 @@ defineExpose({ root })
         <div class="flex flex-col items-center mt-10 gap-1">
             <div v-for="location of locations" class="flex items-center text-white gap-2">
                 <LinkIcon class="h-3" />
-                <div>{{ location }}</div>
+                <a :href="location.url" target="_blank">{{ location.address }}</a>
             </div>
         </div>
         <div class="absolute size-full py-10 flex justify-between pointer-events-none">
